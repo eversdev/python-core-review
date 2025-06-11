e755ba6f4bce0fbba248ac18d2a593da9ef909a0
B <beversprof25@outlook.com>
2025-06-12 00:25:25 +0100

    Implement copy_lines_with_keyword with exception handling and case-insensitive search

--- daily_reviews/q1_copy_lines_with_keyword.py
+++ daily_reviews/q1_copy_lines_with_keyword.py
@@ -1,24 +1,24 @@
-from contextlib import contextmanager #ok does the with statement not work without this import?
 
-
-#creating a function to read the text file
 def copy_lines_with_keyword(filename, keyword):
-    with open(filename, 'r' ) as file:
-        data = file.read()
+    try:
+        with open(filename, 'r' ) as file:
+           file_content = file.read()
+    except FileNotFoundError:
+        print("File not found!")
+        return []
 
-        main_list = data.splitlines()
-        results = []
-        for line in main_list: 
-            new_line = str(line)
-            words = new_line.split()
-            for ele in words:
-                if keyword == ele:
-                    results.append(line)
-                    break
+    main_list = file_content.splitlines()
+    results = []
+    for line in lines: 
+        words_in_line = line.split()
+        for word in words:
+            if keyword == ele:
+                results.append(line)
+                break
     return results
 
             
-print(copy_lines_with_keyword('tester.txt', 'DaTA'))
+print(copy_lines_with_keyword('testerr.txt', 'data'))
 
 
 
